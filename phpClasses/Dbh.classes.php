426b77dfd0fdec2213856e673d66a6263b2511ae
onadiran <onadiranbayonle358@gmail.com>
2022-07-19 11:04:55 +0100

    BlogPost Validations

--- phpClasses/Dbh.classes.php
+++ phpClasses/Dbh.classes.php
@@ -16,3 +16,20 @@ class Dbh{
 
 ?>
 
+<?php 
+// To display image
+$query = "SELECT * FROM blogpost";
+$results = mysqli_query($conn, $query);
+$rows = mysqli_fetch_assoc($results);
+echo $rows['post_image'];
+
+if(mysqli_num_rows($results)> 0){
+    while($rows = mysqli_fetch_assoc($results)){
+        // ?> 
+        <img src="./blogPhotoUpload/<?php //echo $rows['post_image']; ?>" alt="Image will display here">
+        <?php
+    }
+}else{
+    echo 'no image found';
+}
+?>
--- phpClasses/signup.classes.php
+++ phpClasses/signup.classes.php
@@ -1,27 +1,44 @@
 <?php
 
-class signup extends Dbh{
-    protected function checkuser($id, $email){
-        $stmt = $this-> connect()-> prepare('SELECT id from users WHERE id = ? OR email = ?;');
+// class signup extends Dbh{
+//     protected function setUser($name, $email, $dob, $country, $nonNigState, $state, $password){
+//         $stmt = 'INSERT INTO users (name, email, dob, country, nonNigState, state, password)';
+//         $stmt .=  "VALUES ('$name', '$email', '$dob', '$country', '$nonNigState', '$state', '$password') ";
+//         $hashpwd = password_hash($password, PASSWORD_DEFAULT);
+//         if(!$stmt->execute(array($name, $email, $dob, $country, $nonNigState, $state, $password))){
+//             $stmt = null;
+//             header("location: ../register.php?error=stmtfailed");
+//             exit();
+//         }  
+//         $stmt = null;   
+            
+
         
-        $resultCheck;
-        if(!$stmt->execute(array($id, $email))){
-            $stmt = null;
-            header("location: ../register.php?error=stmtfailed");
-            exit();
-        }
 
-        if($stmt ->rowCount() > 0 ){
-            $resultCheck = false;
-        }
-        else{
-            $resultCheck = true;
-        }
+//     protected function checkUser($id, $email){
+//         $stmt = $this-> connect()->prepare('SELECT id from users WHERE id = ? OR email = ?;');
+    
+//         if(!$stmt->execute(array($id, $email))){
+//             $stmt = null;
+//             header("location: ../register.php?error=stmtfailed");
+//             exit();
+//         }
 
-        return $resultCheck;
+//         $resultCheck;
+    
+//         if($stmt->rowCount() > 0 ){
+//             $resultCheck = false;
+//         }
+//         else{
+//             $resultCheck = true;
+//         }
 
+//         return $resultCheck;
+        
+        
+        
+// }
 
-    }
-
-}
 ?>
+
+
